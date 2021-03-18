19e9a92e96b3a4a52978aaa72f66a159ebabf1b4
Lucas Giancola <lucas.giancola@live.com>
2021-03-18 10:56:21 -0400

    Bumped version 20.2 -> 20.3

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=erlang20
 pkg_origin=core
-pkg_version=20.2
+pkg_version=20.3
 pkg_description="A programming language for massively scalable soft real-time systems."
 pkg_upstream_url="http://www.erlang.org/"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://www.erlang.org/download/otp_src_${pkg_version}.tar.gz"
 pkg_filename="otp_src_${pkg_version}.tar.gz"
-pkg_shasum=24d9895e84b800bf0145d6b3042c2f2087eb31780a4a45565206844b41eb8f23
+pkg_shasum=4e19e6c403d5255531c0b870f19511c8b8e3b080618e4f9efcb44d905935b2a1
 pkg_dirname="otp_src_${pkg_version}"
 pkg_build_deps=(
   core/coreutils
